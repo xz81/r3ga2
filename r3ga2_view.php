d7d955b614d6d54abc7be9205faae038a1451d5d
xz81 <xz81@hotmail.com>
2014-05-20 17:08:10 +0200

    asdf

--- r3ga2_view.php
+++ r3ga2_view.php
@@ -18,14 +18,16 @@
 	<div class="r3ga2-title"> r3ga2 controlador de riego </div>
 	<div class="r3ga2-vars">Temp: <span id="temp">250</span> ºC </div>
 	<div class="r3ga2-vars">Hum: <span id="hum">250</span> %HR </div>
+	<div class="r3ga2-title">graph: <span id="graph">250</span> </div>
 </div>
 
 <!-- bring in the emoncms path variable which tells this script what the base URL of emoncms is -->
 <?php global $path; ?>
 
 <!-- feed.js is the feed api helper library, it gives us nice functions to use within our program that
-calls the feed API on the server via AJAX. -->
+calls the feed API on the server via AJAX. I will also bring the multigraph API-->
 <script language="javascript" type="text/javascript" src="<?php echo $path; ?>Modules/feed/feed.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo $path; ?>Modules/vis/multigraph_api.js"></script>
 
 <script>
   // The feed api library requires the emoncms path
@@ -35,6 +37,7 @@ calls the feed API on the server via AJAX. -->
   // Update the elements on the page with the latest power and energy values.
   $("#temp").html(feeds[7]);               // feeds[temp-feed-id]
   $("#hum").html(feeds[8]);                // feeds[hum-feed-id]
+  $("#graph").html(multigraph[1];	   // me lo traerá?
 </script>
 
 <script>
