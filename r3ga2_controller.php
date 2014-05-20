01fec7dfca0f577e0b8fb44ae3ad804a979413dd
root <root@raspberrypi.(none)>
2014-05-20 14:25:58 +0000

    en php ahora

---
+++ r3ga2_controller.php
@@ -0,0 +1,15 @@
+<?php
+
+	// no direct access
+	defined('EMONCMS_EXEC') or die('Restricted access');
+
+	function r3ga2_controller()
+	{
+		global $session,$route;
+		$result = false;
+
+		if ($route->action == "view") $result = view("Modules/r3ga2/r3ga2_view.php",array());
+
+		return array('content'=>$result);
+	}
+
